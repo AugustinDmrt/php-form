89c932411d85f5b89f803f6be848da2960afd971
damoaug <damoaug@ndlp.fr>
2019-10-01 17:15:27 +0200

    [Core] First commuit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    $bdd = mysqli_connect("localhost", "slamquiz", "7pGTNDENkS66tMOz", "slamquiz");
+    $request = ("Select * From tbl_category")
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+    <table>
+        <tr> </tr>
+        <tr> </tr>
+        <tr> </tr>
+    </table>
+
+</body>
+</html>
